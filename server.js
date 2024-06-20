eecd89947b5dde35a0e92398fa93892c0a173833
J0hn316 <jcordell316@gmail.com>
2024-06-20 13:39:59 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
-const PORT = 3001;
-const CardRoute = require("./routes/CardRoutes.js");
+const port = 3001;
+const CardRoute = require('./routes/CardRoutes.js');
 
 dotenv.config();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/cards", CardRoute);
+app.use('/cards', CardRoute);
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Listening on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
